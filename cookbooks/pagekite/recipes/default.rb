c80134d0d737de660803fbcf8a21f6e0e7156cb1
Reuben Garrett <ruby@rubydono.net>
2022-11-21 21:37:50 -0600

    Introduce PageKite cookbook

---
+++ cookbooks/pagekite/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: pagekite
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+
+include_recipe '::install'
---
+++ cookbooks/pagekite/recipes/install.rb
@@ -0,0 +1,6 @@
+
+
+remote_file '/usr/local/bin/pagekite' do
+  source 'https://pagekite.net/pk/pagekite.py'
+  mode   '0555'
+end
